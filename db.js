de8400b1d829d377a20b69c186e4af7b112fea96
Umyt Nuryagdiyev <umyt.nuryagdiyev@gmail.com>
2025-01-11 02:48:04 +0500

    changed host

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const sql = require('mssql');
 const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
+  server: 'localhost',
   database: process.env.DB_NAME,
   options: {
     encrypt: true, // Use this if you're on Windows Azure
